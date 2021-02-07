730e164268c654bada9f0ac0325c40a65a0337b7
Matt <matthew.watts.mw@gmail.com>
2021-02-06 20:58:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
+  const socket = useRef(null);
+  const [retard, setRetard] = useState(0);
+  const [GME, setGME] = useState(0);
+  const [AMC, setAMC] = useState(0);
+
+  useEffect(() => {
+    socket.current = io("http://127.0.0.1:5000");
+    socket.current.on('connect', () => {
+      console.log('connected');
+    });
+    socket.current.onAny((e, arg) => {
+      console.log(e, arg);
+      if (e === 'retard') {
+        setRetard(arg.count);
+      }
+      if (e === 'GME') {
+        setGME(arg.count);
+      }
+      if (e === 'AMC') {
+        setAMC(arg.count);
+      }
+    });
+    return () => {
+      socket.current.close();
+    }
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+        <div>Retard</div>
+        <div>AMC</div>
+        <div>GME</div>
+        <div>{retard}</div>
+        <div>{AMC}</div>
+        <div>{GME}</div>
+    </div>
     </div>
   );
 }
